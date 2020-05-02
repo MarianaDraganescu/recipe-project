f2c5c1e35e80880a383e1f26cb7205d63ae9a649
MarianaDraganescu <draganescu.mariana@yahoo.com>
2020-05-02 13:59:55 +0300

    Added sql file. Closes #7

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories (description) VALUES ('American');
+INSERT INTO categories (description) VALUES ('Italian');
+INSERT INTO categories (description) VALUES ('Mexican');
+INSERT INTO categories (description) VALUES ('Fast food');
+INSERT INTO unitofmeasures (description) VALUES ('spoon');
+INSERT INTO unitofmeasures (description) VALUES ('teaspoon');
+INSERT INTO unitofmeasures (description) VALUES ('cup');
