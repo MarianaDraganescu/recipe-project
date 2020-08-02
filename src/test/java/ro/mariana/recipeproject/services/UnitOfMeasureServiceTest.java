44d758781fcf720b03900c6848e7952bd88ba77a
MarianaDraganescu <draganescu.mariana@yahoo.com>
2020-08-02 22:56:14 +0300

    added working unit test

--- src/test/java/ro/mariana/recipeproject/services/UnitOfMeasureServiceTest.java
+++ src/test/java/ro/mariana/recipeproject/services/UnitOfMeasureServiceTest.java
@@ -5,17 +5,25 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import ro.mariana.recipeproject.converters.UnitOfMeasureEntityToUnitOfMeasureCommand;
+import ro.mariana.recipeproject.dto.UnitOfMeasureCommand;
+import ro.mariana.recipeproject.model.UnitOfMeasureEntity;
 import ro.mariana.recipeproject.repositories.UnitOfMeasureRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class UnitOfMeasureServiceTest {
 
     private UnitOfMeasureService unitOfMeasureService;
 
-    @Mock
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private UnitOfMeasureEntityToUnitOfMeasureCommand unitOfMeasureCommand = new UnitOfMeasureEntityToUnitOfMeasureCommand();
 
     @Mock
-    private UnitOfMeasureEntityToUnitOfMeasureCommand unitOfMeasureCommand;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
 
 
     @BeforeEach
@@ -27,5 +35,20 @@ public class UnitOfMeasureServiceTest {
 
     @Test
     public void listAllUoms() {
+            Set<UnitOfMeasureEntity> unitOfMeasureEntitySet = new HashSet<>();
+            UnitOfMeasureEntity uom1 = new UnitOfMeasureEntity();
+            uom1.setId(1L);
+            unitOfMeasureEntitySet.add(uom1);
+
+            UnitOfMeasureEntity uom2 = new UnitOfMeasureEntity();
+            uom2.setId(2L);
+            unitOfMeasureEntitySet.add(uom2);
+
+            when(unitOfMeasureRepository.findAll()).thenReturn(unitOfMeasureEntitySet);
+
+            Set<UnitOfMeasureCommand> uomsCommands = unitOfMeasureService.listAllUoms();
+
+            assertEquals(2,uomsCommands.size());
+            verify(unitOfMeasureRepository).findAll();
     }
 }
